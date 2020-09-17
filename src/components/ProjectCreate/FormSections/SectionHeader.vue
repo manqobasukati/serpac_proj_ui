f52a0a2c645c0df2df576147e35dff11556c6a7e
manqobasukati <manqoba.sukati@peoplesized.com>
2020-09-17 13:18:07 +0200

    refactored section headers

--- src/components/ProjectCreate/FormSections/SectionHeader.vue
+++ src/components/ProjectCreate/FormSections/SectionHeader.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="text-h6" >{{ section }}</div>
+  <div class="text-h6">{{ sectionInfo.header || 'No header' }}</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+
+import { SectionDescriptions } from 'src/core/Additional/SectionDescriptions';
+
 export default Vue.extend({
   name: 'SectionHeader',
-  props: ['section']
+  props: ['section'],
+
+  computed: {
+    sectionInfo() {
+      return SectionDescriptions[this.section as string];
+    }
+  },
+  mounted() {
+    console.log('Mounted')
+   
+  }
 });
 </script>
---
+++ src/core/Additional/SectionDescriptions.ts
@@ -0,0 +1,32 @@
+export interface SectionDescription {
+  header: string;
+  description?: string;
+}
+
+export const SectionDescriptions: { [name: string]: SectionDescription } = {
+  info_section: {
+    header: 'Information section',
+    description: ''
+  },
+  section_1: {
+    header: 'Section 1 - Provide project description'
+  },
+  section_2: {
+    header: 'Section 2 - Provide project value'
+  },
+  section_3: {
+    header: 'Section 3 - Provide project funding information'
+  },
+  section_4: {
+    header: 'Section 5 - Provide Jobs information'
+  },
+  section_5: {
+    header: 'Section 5 - Provide project timeline info'
+  },
+  section_6: {
+    header: 'Section 6 - Provide MSME Oportunities'
+  },
+  section_7: {
+    header: 'Section 7 - Provide Key enable information'
+  }
+};
